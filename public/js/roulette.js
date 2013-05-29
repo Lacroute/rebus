0f2c6ae82e3289bf25830dfc2ed927adab45d6bf
Procureur <mc.flurry@hotmail.fr>
2013-05-29 17:04:50 +0200

    roulette avec stop et .selected

--- public/js/roulette.js
+++ public/js/roulette.js
@@ -2,15 +2,24 @@ $(document).ready(function(){
 	var hItem = $('.item:first-child').height()
 	var hToScroll = hItem+10+'px';
 	var resultByWord;
+	var selected;
 
 	$('.monte').on('click', function(){
 		resultByWord = $(this).siblings('.resultByWord');
-		if(resultByWord.position().top >= -(resultByWord.height() - hItem) )
+		if(resultByWord.position().top > -(resultByWord.height() - 3*hItem) ){
 			resultByWord.animate({'top': '-='+hToScroll}, 'slow');
+			selected = resultByWord.find('.selected');
+			selected.removeClass('selected');
+			selected.next().addClass('selected');
+		}
 	});
 	$('.descend').on('click', function(){
 		resultByWord = $(this).siblings('.resultByWord');
-		if(resultByWord.position().top <= 0 )
+		if(resultByWord.position().top <= 0 ){
 			resultByWord.animate({'top': '+='+hToScroll}, 'slow');
+			selected = resultByWord.find('.selected');
+			selected.prev().addClass('selected');
+			selected.removeClass('selected');
+		}
 	});
 });
