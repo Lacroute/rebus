067410ab60c7f344a202eafa88bf46d219f5d8e9
u61800734 <benoit.marcanthony@gmail.com>
2013-06-01 18:03:28 +0200

    Installation du mecanisme de decouverte de rebus

--- app/Views/find.php
+++ app/Views/find.php
@@ -2,26 +2,64 @@
 <h1>La phrase</h1>
 <?php //var_dump($empty);?>
 <h1>La phrase</h1>
+<form id="form">
 <?php 
 foreach($sentence as $word){
 	$temp = false;
 	foreach($empty as $a){
 			
-		if(stripos($word, $a->word) !== false){
+		if($word== $a->word){
 			$temp = true;
-
+			
 			break;
 		}
 	}
-	var_dump($temp);
-	if($temp){
-		//echo '<input type="text" value="'.$word.'"/>';
+	
+	if(!$temp){
+		echo '<input type="text" class="check"  data-value="'.$word.'"/> ';
 	}else{
-		//echo '<span>'.$word.' </span>';
+		echo '<span>'.$word.' </span>';
 	}
 
 }
 
 ?>
-
+<input type="submit" style="width:0px;height:0px;border:none;padding:0px;" value="ok"/>
+</form>
+<a href="" id="valid">Check</a>
 </div>
+
+<script>
+	$("#valid").on('click', function(){
+		check();
+		return false;
+
+	});	
+
+	$('#form').on('submit', function(){
+		check();
+		return false;
+	})
+
+
+	function check(){
+		var count = 0;
+		$('.check').each(function(index, obj){
+			console.log($(obj).val().toUpperCase(), $(obj).attr('data-value').toUpperCase());
+			if($.trim($(obj).val().toUpperCase()) != $.trim($(obj).attr('data-value').toUpperCase())){
+				$(obj).css('background-color', 'red');
+			}else{
+				$(obj).css('background-color', 'green');
+				count++;
+			}
+		});
+		if(count == $('.check').length){
+			$('.check').each(function(index, obj){
+				$("<span />", { text: this.value, "class":"view" }).insertAfter(this);
+  				$(this).hide();
+			});
+			console.log('rébus trouvé, TODO : requête en ajax pour valider la chose');
+			$('#valid').remove();
+		}
+	}
+</script>
