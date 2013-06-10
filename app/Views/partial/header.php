fc07af65351d41c26833411146d2c82c577e0f5d
Procureur <mc.flurry@hotmail.fr>
2013-06-10 14:25:08 +0200

    header ok

--- app/Views/partial/header.php
+++ app/Views/partial/header.php
@@ -2,21 +2,20 @@
 <nav>
  	<a id="home" href="home"><img src="public/images/logo.png" alt="Accueil"></a>
 	<ul>
+	<?php if(!$SESSION['user_id']):?>
+		<li><a href="user/login">login</a></li>
+		<li><a href="user/signin">signin</a></li>
+		<li><a href="tutorial"><img src="public/images/info.png" alt="Tutorial"></a></li>
+
+	<?php else:?>
+
+		<li><a href="user/logout">logout</a></li>
 		<li><a href="tutorial"><img src="public/images/info.png" alt="Tutorial"></a></li>
 		<li><a href="user/listing"><img src="public/images/compte.png" alt="Mon compte"></a></li>
 		<li><a href="pool/create"><img src="public/images/kepezz.png" alt="Kepezz"></a></li>
 
-	<?php if(!$SESSION['user_id']):?>
-		<li><a href="user/login">login</a></li>
-		<li><a href="user/signin">signin</a></li>
-
-	<?php else:?>
-
-		<li><a href="user/profile">Profile</a></li>
-		<li><a href="user/logout">logout</a></li>
-
 	<?php endif?>
-	
+
 	</ul>
 
 </nav>
