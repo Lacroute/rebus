f146ec00e304b0577132755df3aa84cce6836e00
u61800734 <benoit.marcanthony@gmail.com>
2013-06-10 17:37:35 +0200

    home bugfix

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,5 +1,5 @@
 <section id="home_images"></section>
-<section id="home_video"><br/><br/><br/><br/>
+<section id="home_video"><br/><br/>
 	<img src="/kep/public/images/logo_big.png" style="margin-left:170px;"><br/><br/><br/>
 	<iframe width="560" height="315" src="http://www.youtube.com/embed/If5MF4wm1T8?rel=0" frameborder="0" allowfullscreen></iframe>
 	<br/>
